abd783a5f1041a0b2f1f9743a13b9051c1535f56
Haley Akin <wasatchtimpanogos@outlook.com>
2023-01-18 10:31:56 -0700

    save

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,51 +1,43 @@
-import React, {useState} from "react";
-import { BiMenu } from "react-icons/bi";
-import { Link } from "react-router-dom";
-
+import React, { useState } from 'react'
+import { BiMenu } from 'react-icons/bi'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false)
 
-     //showMenu
-    const [showMenu, setShowMenu] = useState(false)
+  const toggleMenu = () => {
+    setShowMenu(!showMenu)
+  }
 
-    //handleMenu Function
-    const handleMenu = () => {
-        
-        setShowMenu(!showMenu)
-    }
+  return (
+    <header>
+      <a href='/'>
+        <img
+          className='logo'
+          src='https://cdn.pixabay.com/photo/2023/01/12/21/43/paw-7714934_1280.png'
+          alt='logo'
+        ></img>
+      </a>
 
-    return (
+      <div className='menu-icon-container'>
+        <BiMenu className='menu-icon' onClick={toggleMenu}></BiMenu>
+      </div>
 
-        
-        <header>
+      <nav className={showMenu ? 'menu-show' : 'menu-hide'}>
+        <Link to='/'>
+          <button className='header-btn' onClick={toggleMenu}>
+            Home
+          </button>
+        </Link>
 
-            <img className='header-logo' src='https://cdn.pixabay.com/photo/2023/01/12/21/43/paw-7714934_1280.png' alt='logo'></img>
-                
-        
-            <BiMenu 
-                className="BiMenu"
-                onClick={handleMenu}
-                size={35} 
-                style={{zIndex: 3}}>
-            </BiMenu>
-
-            <nav className={showMenu ? 'menu-show' : 'menu-hide'}>
-
-                <Link to="/">
-                    <button className="header-btn" onClick={handleMenu}>Home</button>
-                </Link>
-
-                <Link to="/profile">
-                    <button className="header-btn" onClick={handleMenu}>Profile</button>
-                </Link>
-
-                
-
-            </nav>
-
-        </header>
-    )
-
+        <Link to='/profile'>
+          <button className='header-btn' onClick={toggleMenu}>
+            Profile
+          </button>
+        </Link>
+      </nav>
+    </header>
+  )
 }
 
-export default Header 
+export default Header
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,64 +1,87 @@
-import React from 'react'
-
-
-
-
-
-
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import Header from '../components/Header'
 
 const Profile = () => {
+  const navigate = useNavigate()
+  const [postBody, setPostBody] = useState({})
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    //setIsLoading(true)
+
+    const data = { body: postBody }
+    const headers = {
+      'Authorization': localStorage.getItem('access-token'),
+      'Content-Type': 'application/json'
+    }
+
+    axios
+      .post('https://haley-capstone.fly.dev/posts', data, {
+        headers
+      })
+      .then(response => {
+        navigate('/')
+      })
+      .catch(error => {
+        // todo: handler errors
+        console.log(error)
+      })
+  }
+
   return (
-    <div className='post-search'>
-   
+    <div>
+      <Header />
+      <div className='post-search'>
+        <section className='posts-container'>
+          <form onSubmit={handleSubmit} className='create-post-form'>
+            <p className='create-post-text'>Create Post:</p>
 
-   
-      
-   
-   
-      
-      
-    <section className='posts-container'>
+            <div className='create-post-container'>
+              <textarea
+                className='create-post-input'
+                cols='30'
+                rows='10'
+                placeholder='Type something...'
+                onChange={e => setPostBody(e.target.value)}
+              ></textarea>
 
-      <form className='create-post-form'>
-        
-        <p className='create-post-text'>Create Post:</p>
+              <input
+                style={{ display: 'none' }}
+                type='file'
+                id='file'
+              />
 
-        <div className='create-post-container'>
+              <label htmlFor='file'>
+                <img
+                  className='add-img-to-post'
+                  src='https://img.icons8.com/ios-glyphs/512/add-image.png'
+                  alt=''
+                />
+              </label>
+            </div>
 
-          <textarea  className="create-post-input" cols="30" rows="10" placeholder='Type something...'></textarea> 
+            <button className='post'>Post</button>
+          </form>
+        </section>
 
-          <input required style={{ display: "none" }} type="file" id="file" />
-
-            <label htmlFor="file">
-
-              <img className="add-img-to-post" src="https://img.icons8.com/ios-glyphs/512/add-image.png" alt="" />
-
-            </label>
-
-        </div>
-
-        <button className='post'>Post</button>
-
-      </form>
-
-    </section>
-
-    <section className='posts-container'>
-
-      <div className='profile-info-container'>
-        <img  className="profile-img"src='https://cdn.pixabay.com/photo/2019/12/08/23/03/pet-4682374_1280.jpg'></img>
-        <p>Fluffy</p>
+        <section className='posts-container'>
+          <div className='profile-info-container'>
+            <img
+              className='profile-img'
+              src='https://cdn.pixabay.com/photo/2019/12/08/23/03/pet-4682374_1280.jpg'
+            ></img>
+            <p>Fluffy</p>
+          </div>
+        </section>
       </div>
-
-    </section>
-
-
-
-
     </div>
   )
 }
 
 export default Profile
+
+
 
 
